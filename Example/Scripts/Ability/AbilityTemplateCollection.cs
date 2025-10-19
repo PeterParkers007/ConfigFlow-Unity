062d7b5812e5fb038e0035d1545f8e287d8b7cf6
PeterParkers007 <3427463164@qq.com>
2025-10-19 18:15:43 +0800

    剔除

--- Example/Scripts/Ability/AbilityTemplateCollection.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using ZJM_JsonTool.Runtime;
-public class AbilityTemplateCollection : TemplateCollection<AbilityTemplate>
-{
-    
-}
